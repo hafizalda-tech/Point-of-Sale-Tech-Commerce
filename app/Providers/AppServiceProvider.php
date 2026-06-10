c0efd40673326e33e5eb2c62323ebed696dcdf26
Hafiz Alda Ghifari <hafizalda.tech@gmail.com>
2026-06-10 14:46:04 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,34 +4,27 @@ namespace App\Providers;
 
 use App\Models\Setting;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         view()->composer('layouts.master', function ($view) {
             $view->with('setting', Setting::first());
         });
+
         view()->composer('layouts.auth', function ($view) {
             $view->with('setting', Setting::first());
         });
+
         view()->composer('auth.login', function ($view) {
             $view->with('setting', Setting::first());
         });
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        URL::forceScheme('https');
     }
 }
